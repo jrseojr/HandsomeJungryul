f83863c7da7f7149c4dc57e9519ebf9cc977b44a
Jungryul Notebook <nospam2@gmail.com>
2017-04-27 17:23:54 +0900

    qqqq

--- Git_tutorial/src/StartPoint.java
+++ Git_tutorial/src/StartPoint.java
@@ -4,6 +4,11 @@ public class StartPoint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Handsome Seo Jungryul");
+		
+		
+		System.out.println("So cooool guy -> Jungryul");
+		
+		System.out.println("UbiLife");
 	}
 
 }
